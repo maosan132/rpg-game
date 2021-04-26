fe2d77e1c9cdd0517a9483d63424d36626ccb017
maosan132 <maosan132@gmail.com>
2021-04-26 14:13:10 -0500

    added asset folder

--- src/Scenes/StartScene.js
+++ src/Scenes/StartScene.js
@@ -9,8 +9,8 @@ export default class StartScene extends Phaser.Scene {
 
   create ()
   {
-    this.add.text(400,250, 'Hello World!');
-    
+    let know = this.add.text(400,250, 'Hello World!');
+    know.setScale(1.5, 1.5)
   }
 
   update()
