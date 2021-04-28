2b6fd11a09b724704232f59afa8b461ddc61d454
maosan132 <maosan132@gmail.com>
2021-04-28 16:54:54 -0500

    Added content to enemy helper

--- src/helpers/Unit.js
+++ src/helpers/Unit.js
@@ -1,20 +1,69 @@
 import 'phaser';
 
-export default const Unit = new Phaser.Class({
+const Unit = new Phaser.Class({
   Extends: Phaser.GameObjects.Sprite,
 
-  initialize:
+  initialize: function Unit(scene, x, y, texture, frame, type, hp, damage) {
+    Phaser.GameObjects.Sprite.call(this, scene, x, y, texture, frame);
+    this.type = type;
+    this.hp = hp;
+    this.maxHp = this.hp;
+    this.damage = damage; // default damage
+  },
 
-  function Unit(scene, x, y, texture, frame, type, hp, damage) {
-      Phaser.GameObjects.Sprite.call(this, scene, x, y, texture, frame)
-      this.type = type;
-      this.maxHp = this.hp = hp;
-      this.damage = damage; // default damage                
+  attack(target) {
+    target.takeDamage(this.damage);
   },
-  attack: function(target) {
-      target.takeDamage(this.damage);      
-  },
-  takeDamage: function(damage) {
-      this.hp -= damage;        
+
+  takeDamage(damage) {
+    this.hp -= damage;
   }
 });
+
+const Enemy = new Phaser.Class({
+  Extends: Unit,
+
+  initialize: function Enemy(scene, x, y, texture, frame, type, hp, damage) {
+    Unit.call(this, scene, x, y, texture, frame, type, hp, damage);
+  },
+});
+
+
+/***************************************************** */
+export default class Unit extends Phaser.GameObjects.Sprite {
+
+  constructor() {
+    super();
+    this.type = type;
+    this.hp = hp;
+    this.maxHp = this.hp;
+    this.damage = damage; // default damage
+  }
+
+  attack(target) {
+    target.takeDamage(this.damage)
+  }
+
+  takeDamage(damage) {
+    this.hp -= damage;
+  }
+
+}
+
+/***************************************************** */
+
+
+export default class FightScene extends Phaser.Scene {
+  preload() {}
+
+  create() {
+    // Run UserScene parallelly
+    this.scene.launch('UserScene');
+
+    // Set background to green color
+    this.cameras.main.setBackgroundColor('rgba(200, 100, 120, 0.5)');
+  }
+
+  update() {
+  }
+}
