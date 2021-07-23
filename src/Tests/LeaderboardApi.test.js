a63159d2525d4e7417f9d5ddc2b933e7f4967f3f
maosan132 <maosan132@gmail.com>
2021-07-22 21:33:50 -0500

    Add more tests

--- src/Tests/LeaderboardApi.test.js
+++ src/Tests/LeaderboardApi.test.js
@@ -1,4 +1,4 @@
-import { putScore, getScores } from '../Config/LeaderboardApi';
+import { putScore, getScores, createGame} from '../Config/LeaderboardApi';
 
 describe('Scores should be written and read from the API', () => {
   it('Should post the score', () => {
@@ -10,6 +10,22 @@ describe('Scores should be written and read from the API', () => {
     getScores().then(data => {
       expect(typeof data).toBe('object');
       expect(data.result).toContainEqual('user');
+    });
+  });
+});
+
+describe('Should create a game with a valid name', () => {
+  it('create a game to if the name is valid', () => {
+    createGame().then((data) => {
+      expect(data).toBeTruthy();
+    });
+  });
+});
+
+describe('Should retrieve the score', () => {
+  it('return the score if the app exists', () => {
+    getScores().then((data) => {
+      expect(typeof data).toBe('object');
     });
   });
 });
--- src/Tests/score.test.js
+++ src/Tests/score.test.js
@@ -10,4 +10,8 @@ describe('The scores should be set and read', () => {
     InitSettings.score = 60;
     expect(InitSettings.score).toBe(60);
   });
+  it('Should set the correct score', () => {
+    InitSettings.score = 80;
+    expect(InitSettings.score).not.toBe(100);
+  });
 });
